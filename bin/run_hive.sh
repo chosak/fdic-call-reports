8eae79f0db752a8f47c48e389eeb7013cc2519ee
Andy Chosak <chosak@gmail.com>
2015-07-12 22:25:49 -0400

    cleanup

--- bin/run_hive.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-
-export HIVE_OPTS=" \
-    --hiveconf fs.s3n.awsAccessKeyId=$AWS_ACCESS_KEY_ID \
-    --hiveconf fs.s3n.awsSecretAccessKey=$AWS_SECRET_ACCESS_KEY \
-"
-
-hive $@
